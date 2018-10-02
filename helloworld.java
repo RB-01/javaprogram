5c1c60d127b940a51a791e318cca1af7419cc88c
RB-01 <rohitbhatia964@gmail.com>
2018-10-03 03:06:42 +0530

    First java program for git

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld{
+
+  public static void main(string[] args)   {
+            //prints "hello,world" to the terminal window.
+             system.out.println("hello,world");
+     }
+
+}
